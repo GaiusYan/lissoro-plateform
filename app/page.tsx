85ba0c366d0bb7b0bde232e14c514e206ccfbc20
GaiusYan <gaiusyanbena11@gmail.com>
2026-03-16 14:18:34 +0100

    install prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 
-import {Button} from "@/components/ui/button";
 
 export default function Home() {
+ 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button >Button</Button>
-      <Button>Another button</Button>
+    <div>
+      Lissoro application
     </div>
   );
 }
